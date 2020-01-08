9b0c8a49b87b46347b0778370e326e7769848aae
EricV91 <ericverhoeff91@hotmail.com>
2020-01-08 11:55:55 +0100

    restored old version of test_pca

--- test_pca.py
+++ test_pca.py
@@ -3,13 +3,17 @@
 #%% 
 #import packages 
 
-
-
 import os
 import pptk
 import numpy as np
 from laspy.file import File
 import math
+import numba
+from numba import jit
+from skimage.color import rgb2lab
+import cv2
+from PIL import Image
+from sklearn.preprocessing import MinMaxScaler
 
 #%%
 
@@ -22,34 +26,107 @@ for root, dirs, files in os.walk(folder, topdown=True):
         if name[-4:] == ".las":
             las_files.append(os.path.join(root,name).replace("\\","/"))
             
-data_las = File(las_files[39], mode='r') #39 has big differences
+data_las = File(las_files[45], mode='r') #39 has big differences
 
 xyz = np.vstack([data_las.X, data_las.Y, data_las.Z]).transpose()
-rgb = ((np.c_[data_las.Red, data_las.Green, data_las.Blue]) / 255.) / 255.
+rgb = ((np.c_[data_las.Red, data_las.Green, data_las.Blue]) / 255.) / 255. #normalized
+R = rgb[:,0]
+G = rgb[:,1]
+B = rgb[:,2]
 x = xyz[:,0]
 y = xyz[:,1]
 z = xyz[:,2]
 
 #remove the median and divide by std = standarlize the data
-# =============================================================================
-# median = np.median(a=xyz,axis=0)
-# std = np.std(a=xyz,axis=0)
-# xn = (x - median[0])/std[0]
-# yn = (y - median[1])/std[1]
-# zn = (z - median[2])/std[2]
-# xyz_new = np.vstack([xn,yn,zn]).T
-# =============================================================================
-
-#extract the mean from the data
+#We standarlize the data because...https://stats.stackexchange.com/questions/287425/why-do-you-need-to-scale-data-in-knn
 median = np.median(a=xyz,axis=0)
 std = np.std(a=xyz,axis=0)
-xn = x - median[0]
-yn = y - median[1]
-zn = z - median[2]
-xyz_new = np.vstack([xn,yn,zn]).T
-#visualize the data
-#v = pptk.viewer(xyz_new,rgb)
+x_std = (x - median[0])/std[0]
+y_std = (y - median[1])/std[1]
+z_std = (z - median[2])/std[2]
+xyz_std = np.vstack([x_std,y_std,z_std]).T
 
+median_rgb = np.median(a=rgb,axis=0)
+std_rgb = np.std(a=rgb,axis=0)
+r_std = (R - median_rgb[0])/std_rgb[0]
+g_std = (G - median_rgb[1])/std_rgb[1]
+b_std = (B - median_rgb[2])/std_rgb[2]
+rgb_std = np.vstack([r_std,g_std,b_std]).T
+
+#normalize the rgb
+r_n = (R - R.min()) / (R.max() - R.min())
+g_n = (G - G.min()) / (G.max() - G.min())
+b_n = (B - B.min()) / (B.max() - B.min())
+rgb_n = np.vstack([r_n,g_n,b_n]).T
+
+# from BGR to Lab # https://gist.github.com/bikz05/6fd21c812ef6ebac66e1
+def func(t):
+    if (t > 0.008856):
+        return np.power(t, 1/3.0);
+    else:
+        return 7.787 * t + 16 / 116.0;
+
+#Conversion Matrix
+matrix = [[0.412453, 0.357580, 0.180423],
+          [0.212671, 0.715160, 0.072169],
+          [0.019334, 0.119193, 0.950227]]
+
+# RGB values lie between 0 to 1.0
+Lab_OpenCv = []
+Lab = np.zeros((len(rgb),3))
+for row in rgb:
+    cie = np.dot(matrix, row);
+    
+    cie[0] = cie[0] /0.950456;
+    cie[2] = cie[2] /1.088754; 
+    
+    # Calculate the L
+    L = 116 * np.power(cie[1], 1/3.0) - 16.0 if cie[1] > 0.008856 else 903.3 * cie[1];
+    
+    # Calculate the a 
+    a = 500*(func(cie[0]) - func(cie[1]));
+    
+    # Calculate the b
+    b = 200*(func(cie[1]) - func(cie[2]));
+    
+    #  Values lie between -128 < b <= 127, -128 < a <= 127, 0 <= L <= 100 
+    Lab = [b , a, L]; 
+    
+    # OpenCV Format
+    L = L * 255 / 100;
+    a = a + 128;
+    b = b + 128;
+    Lab_OpenCv.append([b,a,L])
+Lab_OpenCv = np.asarray(Lab_OpenCv)
+scaler = MinMaxScaler()
+Lab = scaler.fit_transform(Lab_OpenCv)
+Lab[:,2] = 0.0
+b = Lab[:,0]
+a = Lab[:,1]
+L = Lab[:,2]
+# =============================================================================
+# BGR = np.zeros((len(rgb),1,3))
+# B = np.reshape(B,(len(B),1))
+# G = np.reshape(G,(len(G),1))
+# R = np.reshape(R,(len(R),1))
+# 
+# BGR[:,:,0] = B
+# BGR[:,:,1] = G
+# BGR[:,:,2] = R
+# 
+# conv= cv2.cvtColor(BGR, cv2.COLOR_BGR2LAB)
+# 
+#     
+# d = 0
+# for row in rgb:
+#     i = row*255
+#     ii = (int(i[0]),int(i[1]),int(i[2]))  
+#     img = Image.new('RGB',(1,1),color = ii)
+#     img.save(r'C:\Users\laptop\Desktop\pixels\%d.png'%d)
+#     d+=1
+# =============================================================================
+
+    
 #######xyz_new = np.concatenate((xyz_new,rgb),axis = 1) #if we want to put the RGB as well
 #%%
 #10m resolution of broccoli crops
@@ -68,17 +145,40 @@ xyz_new = np.vstack([xn,yn,zn]).T
 folder_10m = r"C:\Users\laptop\Google Drive\Shared folder Tasos-VanBoven\Sample_data\Broccoli\AZ74_10m-0-1 - Cloud.las"
 data_las = File(folder_10m, mode = 'r')
 xyz = np.vstack([data_las.X, data_las.Y, data_las.Z]).transpose()
+rgb = ((np.c_[data_las.Red, data_las.Green, data_las.Blue]) / 255.) / 255.
+R = rgb[:,0]
+G = rgb[:,1]
+B = rgb[:,2]
+
 x = xyz[:,0]
 y = xyz[:,1]
 z = xyz[:,2]
 
 #remove the median 
-median = np.median(a=xyz,axis=0)
-xn = x - median[0]
-yn = y - median[1]
-zn = z - median[2]
-xyz_new = np.vstack([xn,yn,zn]).T
+# =============================================================================
+# median = np.median(a=xyz,axis=0)
+# xn = x - median[0]
+# yn = y - median[1]
+# zn = z - median[2]
+# xyz_new = np.vstack([xn,yn,zn]).T
+# =============================================================================
 
+#Standarlize the data
+#We standarlize the data because...https://stats.stackexchange.com/questions/287425/why-do-you-need-to-scale-data-in-knn
+
+median = np.median(a=xyz,axis=0)
+std = np.std(a=xyz,axis=0)
+x_std = (x - median[0])/std[0]
+y_std = (y - median[1])/std[1]
+z_std = (z - median[2])/std[2]
+xyz_std = np.vstack([x_std,y_std,z_std]).T
+
+median_rgb = np.median(a=rgb,axis=0)
+std_rgb = np.std(a=rgb,axis=0)
+r_std = (R - median_rgb[0])/std_rgb[0]
+g_std = (G - median_rgb[1])/std_rgb[1]
+b_std = (B - median_rgb[2])/std_rgb[2]
+rgb_std = np.vstack([r_std,g_std,b_std]).T
 
 
 
@@ -88,8 +188,8 @@ xyz_new = np.vstack([xn,yn,zn]).T
 
 
 from sklearn.neighbors import NearestNeighbors #import NearestNeigbhors package
-nbrs = NearestNeighbors(n_neighbors = 35, algorithm = 'kd_tree').fit(xyz_new) #['auto', 'ball_tree', 'kd_tree', 'brute']
-distances, indices = nbrs.kneighbors(xyz_new) #the indices of the nearest neighbors 
+nbrs = NearestNeighbors(n_neighbors = 35, algorithm = 'kd_tree').fit(xyz_std) #['auto', 'ball_tree', 'kd_tree', 'brute']
+distances, indices = nbrs.kneighbors(xyz_std) #the indices of the nearest neighbors 
 
 
 
@@ -102,15 +202,16 @@ planarity = []
 scatter = []
 omnivariance = []
 anisotropy = []
-#eigenentropy = []
+eigenentropy = []
 change_curvature = []
 dif_elev = []
 mean_elev = []
 omnivariance = []
 
+#@jit(nopython=True)
 for i in range(len(indices)):
     ind = indices[i]
-    coords = xyz_new[(ind),:]
+    coords = xyz_std[(ind),:]
     x = coords[:,0]
     y = coords[:,1]
     z = coords[:,2]
@@ -136,10 +237,8 @@ for i in range(len(indices)):
     scatter.append(sc)
     anis = (e[0]-e[2])/e[0]
     anisotropy.append(anis)
-# =============================================================================
-#     ei = -(e[0]*math.log(e[0])+e[1]*math.log(e[1])+e[2]*math.log(e[2]))
-#     eigenentropy.append(ei)
-# =============================================================================
+    ei = -(e[0]*math.log(e[0])+e[1]*math.log(e[1])+e[2]*math.log(e[2]))
+    eigenentropy.append(ei)
     cha = e[2]/sum(e)
     change_curvature.append(cha)
     m_el = z.mean()
@@ -147,11 +246,8 @@ for i in range(len(indices)):
     d_el = z.max()-z.min()
     dif_elev.append(d_el)
      
-    
-
-   
-    
 #normalization
+#https://stats.stackexchange.com/questions/69157/why-do-we-need-to-normalize-data-before-principal-component-analysis-pca
 omnivariance = np.asarray(omnivariance)
 omn_n = (omnivariance -omnivariance.min()) / (omnivariance.max() - omnivariance.min())
 l = np.asarray(linearity)
@@ -162,10 +258,8 @@ s = np.asarray(scatter)
 scat_n = (s -s.min()) / (s.max() - s.min())
 an = np.asarray(anisotropy)
 an_n = (an -an.min()) / (an.max() - an.min())
-# =============================================================================
-# eig = np.asarray(eigenentropy)
-# eig_n = (eig -eig.min()) / (eig.max() - eig.min())
-# =============================================================================
+eig = np.asarray(eigenentropy)
+eig_n = (eig -eig.min()) / (eig.max() - eig.min())
 ch = np.asarray(change_curvature)
 ch_cur_n = (ch -ch.min()) / (ch.max() - ch.min())
 m_e = np.asarray(mean_elev)
@@ -173,92 +267,131 @@ mean_el_n = (m_e -m_e.min()) / (m_e.max() - m_e.min())
 d_e = np.asarray(dif_elev)
 dif_elev_n = (d_e -d_e.min()) / (d_e.max() - d_e.min())
 
-
+#standardization is used for PCA? preprocessing.StandardScaler does not work for me, hence manually..
+omn_std = (omnivariance-omnivariance.mean())/omnivariance.std()
+lin_std = (l-l.mean())/l.std()
+plan_std = (p-p.mean())/p.std()
+scat_std = (s-s.mean())/s.std()
+an_std = (an-an.mean())/an.std()
+eig_std = (eig-eig.mean())/eig.std()
+ch_cur_std = (ch-ch.mean())/ch.std()
+mean_el_std = (m_e-m_e.mean())/m_e.std()
+dif_elev_std = (d_e-d_e.mean())/d_e.std()
 
 #visualization
-v = pptk.viewer(xyz,lin_n)
-v.set(point_size=7)
-v.capture('Linearity.png')
-
-v = pptk.viewer(xyz,plan_n)
-v.set(point_size=7)
-v.capture('Planarity.png')
-
-v = pptk.viewer(xyz,scat_n)
-v.set(point_size=7)
-v.capture('Scattering.png')
-
-v = pptk.viewer(xyz,an_n)
-v.set(point_size=7)
-v.capture('Anisotropy.png')
-
-v = pptk.viewer(xyz,eig_n)
-v.set(point_size=10)
-v.capture('Eigenotropy.png')
-
-
-v = pptk.viewer(xyz,ch_cur_n)
-v.set(point_size=7)
-v.capture('Change_of_Curvature.png')
-
-v = pptk.viewer(xyz_new,mean_el_n)
-v.set(point_size=7)
-v.capture('Mean_elevation.png')
-
-v = pptk.viewer(xyz_new,dif_elev_n)
-v.set(point_size=7)
-v.capture('Elevation_Difference.png')
-
-v = pptk.viewer(xyz_new,omn_n)
-v.set(point_size=7)
-v.capture('Omnivariance.png')
-
-# Not performing good when normalize the data with (MinMaxScaler)!!!
-    
-
 # =============================================================================
-# v = pptk.viewer(xyz,lin_n)
+# v = pptk.viewer(xyz,lin_std)
 # v.set(point_size=7)
 # v.capture('Linearity.png')
 # 
-# v = pptk.viewer(xyz,plan_n)
+# v = pptk.viewer(xyz,plan_std)
 # v.set(point_size=7)
 # v.capture('Planarity.png')
 # 
-# v = pptk.viewer(xyz,scat_n)
+# v = pptk.viewer(xyz,scat_std)
 # v.set(point_size=7)
 # v.capture('Scattering.png')
 # 
-# v = pptk.viewer(xyz,an_n)
+# v = pptk.viewer(xyz,an_std)
 # v.set(point_size=7)
 # v.capture('Anisotropy.png')
 # 
-# v = pptk.viewer(xyz,eig_n)
-# v.set(point_size=10)
-# v.capture('Eigenotropy.png')
+# # =============================================================================
+# # v = pptk.viewer(xyz,eig_std)
+# # v.set(point_size=10)
+# # v.capture('Eigenotropy.png')
+# # =============================================================================
 # 
 # 
-# v = pptk.viewer(xyz,ch_cur_n)
+# v = pptk.viewer(xyz,ch_cur_std)
 # v.set(point_size=7)
 # v.capture('Change_of_Curvature.png')
 # 
-# v = pptk.viewer(xyz,mean_el_n)
-# v.set(point_size=1)
+# v = pptk.viewer(xyz,mean_el_std)
+# v.set(point_size=7)
 # v.capture('Mean_elevation.png')
 # 
-# v = pptk.viewer(xyz,dif_elev_n)
+# v = pptk.viewer(xyz,dif_elev_std)
 # v.set(point_size=7)
 # v.capture('Elevation_Difference.png')
 # 
-# v = pptk.viewer(xyz,omn_n)
+# v = pptk.viewer(xyz,omn_std)
 # v.set(point_size=7)
 # v.capture('Omnivariance.png')
 # =============================================================================
 
-    
+#%% lecture 7 decession trees and pca (geoprocessing analysis)
+#pca for the geometrical features to define the most important features with minmaxscaler features
+# https://www.visiondummy.com/2014/05/feature-extraction-using-pca/
+# https://chrisalbon.com/machine_learning/feature_engineering/feature_extraction_with_pca/ 
+# https://towardsdatascience.com/an-approach-to-choosing-the-number-of-components-in-a-principal-component-analysis-pca-3b9f3d6e73fe
 
-    
-    
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+#features = np.vstack((omn_std, lin_std, plan_std, scat_std, an_std, eig_std, ch_cur_std, mean_el_std, dif_elev_std, r_std, g_std, b_std)).T
+features = np.vstack((omn_n, lin_n, plan_n, scat_n, an_n, eig_n, ch_cur_n, mean_el_n, dif_elev_n, r_n, g_n, b_n)).T #normalize
+#features = np.vstack((omn_n, lin_n, plan_n, scat_n, an_n, eig_n, ch_cur_n, mean_el_n, dif_elev_n, L, a, b)).T #normalize
+features = np.vstack((omn_n, lin_n, plan_n, scat_n, an_n, eig_n, ch_cur_n, mean_el_n, dif_elev_n, R, G, B)).T
+features = np.vstack((omn_n, dif_elev_n, R, G, B)).T # till now the best combination of features!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+features = np.vstack((omn_n, dif_elev_n, L, a, b)).T #Lab
 
 
 
+
+
+
+
+#data = np.concatenate((features),axis=0)
+scaler = MinMaxScaler(feature_range=[0,1])
+data_rescaled = scaler.fit_transform(features)
+pca = PCA().fit(data_rescaled)
+#pca = PCA().fit(features)
+
+plt.figure()
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.xlabel('Number of Features')
+plt.ylabel('Variance (%)')
+plt.title('Subjective Choose of Features')
+plt.show()
+
+# we already have normalized data hence perhaps the minmaxscaler is redundant 
+pca1 = PCA().fit(features)
+plt.figure()
+plt.plot(np.cumsum(pca1.explained_variance_ratio_))
+plt.xlabel('Number of Features')
+plt.ylabel('Variance (%)')
+plt.title('Subjective Choose of Features')
+plt.show()
+
+#hence we can keep 5 attributes/features that have the variance of almost all the data ~100%
+pca2 = PCA(n_components=5)
+features_new = pca2.fit_transform(features)
+
+
+#%%
+#pca2with standarlized data
+# https://chrisalbon.com/machine_learning/feature_engineering/feature_extraction_with_pca/
+# https://medium.com/apprentice-journal/pca-application-in-machine-learning-4827c07a61db
+# https://towardsdatascience.com/principal-component-analysis-for-dimensionality-reduction-115a3d157bad
+
+from sklearn import decomposition, datasets
+from sklearn.preprocessing import StandardScaler
+features = np.vstack((omn_std, lin_std, plan_std, scat_std, an_std, eig_std, ch_cur_std, mean_el_std, dif_elev_std, r_std, g_std, b_std)).T
+features = np.asarray(features)
+sc = StandardScaler()
+#fit the scaler to the features and transform
+features_std = sc.fit_transform(features)
+#create a pca object with the 8 components as parameter
+pca = decomposition.PCA(n_components=5)
+#fit the PCA and transform the data
+features_std_pca = pca.fit_transform(features_std)
+features_std_pca.shape
+
+
+plt.figure()
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.xlabel('Number of Features')
+plt.ylabel('Variance (%)')
+plt.title('Subjective Choose of Features')
+plt.show()
