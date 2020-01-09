af9bc0c62a8454695898804bac11735b399f7209
Anastasios Vogiatzis <57905327+Tasos89@users.noreply.github.com>
2020-01-09 11:46:52 +0100

    Update Create_training_data.py

--- Create_training_data.py
+++ Create_training_data.py
@@ -11,7 +11,7 @@ ToDo:
 1. read plant shapes in geodataframe 
 2. read point cloud in numpy array
 3. convert pointcloud to geodataframe
-4. select points in plant polygons using contains
+4. select points coinside with plant polygons using contains
 5. label selected points as 1 for broccoli and include column with plant id
 6. export np array as las file.
 
@@ -22,6 +22,8 @@ import numpy as np
 import geopandas as gpd
 import pandas as pd
 from laspy.file import File
+import laspy.header
+
 
 #plant shape path
 input_plant_path = r"D:\New folder\Nieuwe map\merged_shapes.shp"
@@ -50,18 +52,20 @@ for file in las_files[:5]:
 xyz = np.concatenate(las_list, axis = 0)
 
 #convert array into dataframe
-df = pd.DataFrame(xyz, columns=['X','Y', 'Z'])
+df = pd.DataFrame(xyz, columns=['X','Y','Z'])
 
-#convert to geodataframe
+#convert to geodataframe # https://geopandas.readthedocs.io/en/latest/gallery/create_geopandas_from_pandas.html
 gdf = gpd.GeoDataFrame(
     df, geometry=gpd.points_from_xy(df.X, df.Y))
 
+#Initialize the reference system of the points
 #gdf.crs = {'init' :'epsg:28992'}
 gdf.crs = ({'init' :'epsg:4326'})
 #gdf = gdf.to_crs({'init' :'epsg:28992'})
 
 gdf['plant_id'] = 0
 
+#Coinside of points and polygons shapes
 #for i, plant in enumerate(plant_shapes):
 contained = plant_shapes.geometry.contains(gdf.geometry)
 contained2 = gdf.geometry.contains(plant_shapes.geometry)
@@ -82,3 +86,12 @@ np.save('broccoli_points', broccoli_points)
 
 #gdf.iloc[:].to_file(output_folder + r'\points_broccoli.gpkg')
 
+#convert contained broccoli points from geodataframe to .las and save it
+outfile = laspy.file.File(r'G:\Shared drives\400 Development\Student Assignments\Interns\Tasos\Sample_data\Broccoli\broccoli.las', mode="w", header=header)
+outfile.X = contained[:,0]
+outfile.Y = contained[:,1]
+outfile.Z = contained[:,2]
+outfile.Red = contained[:,3]
+outfile.Greeen = contained[:,4]
+outfile.Blue = contained[:,5]
+outfile.colse()
